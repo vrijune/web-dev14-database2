c98f2fb052e152edfed6c28ee3ef4ab376829b17
Wen YU <wyu861@aucklanduni.ac.nz>
2021-01-13 16:38:49 +1300

    web-lab-14\

--- sql/ictgradschool/web/lab14/ex1/ex1.sql
+++ sql/ictgradschool/web/lab14/ex1/ex1.sql
@@ -1,1 +1,35 @@
 -- Your SQL for question 1
+CREATE TABLE employees (
+    fullname VARCHAR(50) not null,
+
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE departments();
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE vehicles();
